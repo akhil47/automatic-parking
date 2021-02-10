bf54e7738b2fb600739c8c806508eb60496bacbb
Akhilesh Lingala <akhil@Akhileshs-MacBook-Pro.local>
2021-02-10 11:21:16 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ParkingDetailsComponent } from './parking-details/parking-details.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes = [
+  {path: '', redirectTo: 'create-parking-lot', pathMatch: 'full'},
+  {path: 'create-parking-lot', component: LandingPageComponent},
+  {path: 'parking-lot', component: ParkingDetailsComponent},
+  {path: '**', component: PageNotFoundComponent}
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    ParkingDetailsComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  createNewParkingLot(form: NgForm){
+    console.log(form.value)
+    this.router.navigate(['/parking-lot'])
+  }
+}
